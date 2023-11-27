e993e9d94e29e58f7aeb5bce44b8b1f1fb614702
Matt Saemrow <mattsaemrow@gmail.com>
2023-11-27 14:51:34 -0600

    search bar is function. need to add cdd

--- script.js
+++ script.js
@@ -3,25 +3,37 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function search(str) {
+function search() {
 	let results = [];
 
-	// TODO
-
+	let searchStr = input.value.toLowerCase();
+	for (let name of fruit){
+		if(name.toLowerCase().includes(searchStr)){
+			results.push(name);
+		}
+	}
+	console.log(results);
 	return results;
 }
 
 function searchHandler(e) {
-	// TODO
+	showSuggestions(search(),input.value);
 }
 
 function showSuggestions(results, inputVal) {
-
-	// TODO
+	suggestions.innerHTML = '';
+	for (let fruit of results){
+		const searchResult = document.createElement('li');
+		searchResult.innerText = fruit;
+		searchResult.classList.add('result');
+		suggestions.appendChild(searchResult);
+	}
 }
 
 function useSuggestion(e) {
-	// TODO
+	let searchTerm = e.target.innerText;
+	input.value = searchTerm;
+	suggestions.innerHTML = '';
 }
 
 input.addEventListener('keyup', searchHandler);
