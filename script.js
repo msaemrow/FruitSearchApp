07471684e71bf6c682d7e7661ced96ea3c240acf
Matt Saemrow <mattsaemrow@gmail.com>
2023-11-28 10:15:05 -0600

    fixed css and fixed bugs for end cases

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const input = document.querySelector('#fruit');
-const suggestions = document.querySelector('.suggestions ul');
+const input = document.querySelector('#input-box');
+const suggestions = document.querySelector('.suggestions');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
@@ -9,28 +9,28 @@ function search() {
 	for (let name of fruit){
 		if(name.toLowerCase().includes(searchStr)){
 			results.push(name);
-		}
+		} 
 	}
-	console.log(results);
 	return results;
 }
 
 function searchHandler(e) {
-	if(e.target.value === ''){
-		suggestions.innerHTML = '';
-		return;
-	} else {
 		showSuggestions(search(),input.value);
-	}
 }
 
 function showSuggestions(results, inputVal) {
 	suggestions.innerHTML = '';
+	if(inputVal === '' || results.length === 0){
+		return;
+	} else{
+	const searchList = document.createElement('ul');
 	for (let fruit of results){
 		const searchResult = document.createElement('li');
 		searchResult.innerText = fruit;
 		searchResult.classList.add('result');
-		suggestions.appendChild(searchResult);
+		searchList.appendChild(searchResult);
+	}
+	suggestions.appendChild(searchList);
 	}
 }
 
